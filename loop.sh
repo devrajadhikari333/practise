bb2b12f7a57e537100cee495b694683fd0733d77
Devraj Adhikari <devrajadhikari333@gmail.com>
2020-02-09 20:52:16 +0000

    modified from Development

--- loop.sh
+++ loop.sh
@@ -2,6 +2,7 @@
 clear
 a=1
 while [[ $a -le 10 ]]; do
-       echo "this is my profile" 
+       echo "this is my profile"
+      echo "this is personal please do not access it" 
 a=$a+1       
 done
